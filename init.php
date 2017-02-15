aa9948479e311fb8d04da62324cfd7175bebcbd6
Carlo J <422247668@qq.com>
2017-02-15 14:07:16 +0800

    [fixed] fixed init.php require_once

--- init.php
+++ init.php
@@ -5,17 +5,17 @@ date_default_timezone_set('PRC');
 //error_reporting(0);
 header('Content-Type:text/html;charset=utf-8');
 define('ROOT_PATH', str_replace("\\",'/',dirname(__FILE__)));
-require_once(ROOT_PATH . '/lib/const.inc.php');
+require(ROOT_PATH . '/lib/const.inc.php');
 
 if(file_exists(ROOT_PATH . '/config/config.php')){
 	define('CONFIG_PATH',ROOT_PATH . '/config/');
 }
-$config = require_once(CONFIG_PATH.'/config.php');
+$config = require(CONFIG_PATH.'/config.php');
 $domain_tld = $config['tld'];
 
-require_once(ROOT_PATH . '/lib/common.fun.php');
-require_once(ROOT_PATH . '/lib/phpQuery.class.php');
-require_once (ROOT_PATH . '/lib/db.class.php');
+require(ROOT_PATH . '/lib/common.fun.php');
+require(ROOT_PATH . '/lib/phpQuery.class.php');
+require(ROOT_PATH . '/lib/db.class.php');
 
 foreach ($config['db'] as $once => $cfg){
     $db[$once] = new db($once);
